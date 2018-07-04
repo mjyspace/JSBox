ea31fca532b667b8e34911b506a0e0a14d86f8e0
Junyu <mjyspace@gmail.com>
2018-07-04 08:13:11 +0800

    1、地图显示区域调整，UI 优化。
    2、对于缺失 EXIF 的图片（例如 GPS 等信息），允许自行添加。

--- EXIF Modify/main.js
+++ EXIF Modify/main.js
@@ -14,6 +14,192 @@ let deviceScreenWidth = $device.info["screen"]["width"];
 
 let input_element_height = deviceScreenHeight / 15 - 10;
 
+let default_exifObj = {
+    "Interop": {},
+    "GPS": {
+        "2": [
+            [
+                1,
+                1
+            ],
+            [
+                1,
+                1
+            ],
+            [
+                1111,
+                100
+            ]
+        ],
+        "3": "E",
+        "16": "T",
+        "4": [
+            [
+                1,
+                1
+            ],
+            [
+                1,
+                1
+            ],
+            [
+                1111,
+                100
+            ]
+        ],
+        "5": 0,
+        "12": "K",
+        "23": "T",
+        "6": [
+            1,
+            1
+        ],
+        "17": [
+            1,
+            1
+        ],
+        "13": [
+            0,
+            1
+        ],
+        "7": [
+            [
+                1,
+                1
+            ],
+            [
+                1,
+                1
+            ],
+            [
+                1,
+                100
+            ]
+        ],
+        "24": [
+            1,
+            1
+        ],
+        "29": "1970:01:01",
+        "1": "N",
+        "31": [
+            1,
+            1
+        ]
+    },
+    "0th": {
+        "34853": 1,
+        "271": "",
+        "274": 0,
+        "531": 1,
+        "306": "1970:01:01 01:01:01",
+        "283": [
+            1,
+            1
+        ],
+        "296": 2,
+        "305": "12.0",
+        "282": [
+            72,
+            1
+        ],
+        "272": "",
+        "34665": 1
+    },
+    "Exif": {
+        "41987": 0,
+        "41985": 2,
+        "37379": [
+            1,
+            1
+        ],
+        "37386": [
+            1,
+            1
+        ],
+        "37377": [
+            1,
+            1
+        ],
+        "41990": 0,
+        "40962": 1,
+        "37522": "",
+        "40960": "0100",
+        "36867": "1970:01:01 01:01:01",
+        "37380": [
+            0,
+            1
+        ],
+        "42036": "",
+        "41495": 1,
+        "37500": "",
+        "42034": [
+            [
+                1,
+                1
+            ],
+            [
+                1,
+                1
+            ],
+            [
+                1,
+                1
+            ],
+            [
+                1,
+                1
+            ]
+        ],
+        "37121": "",
+        "34850": 1,
+        "41986": 0,
+        "33434": [
+            1,
+            2
+        ],
+        "41729": "",
+        "37396": [
+            2,
+            1,
+            2,
+            1
+        ],
+        "37378": [
+            1,
+            1
+        ],
+        "40963": 1,
+        "37385": 1,
+        "37383": 0,
+        "36868": "1970:01:01 01:01:01",
+        "37521": "",
+        "34855": 1,
+        "40961": 65535,
+        "42035": "",
+        "36864": "",
+        "33437": [
+            9,
+            5
+        ],
+        "41989": 1
+    },
+    "1st": {
+        "296": 0,
+        "259": 0,
+        "513": 1,
+        "282": [
+            1,
+            1
+        ],
+        "514": 1,
+        "283": [
+            1,
+            1
+        ]
+    }
+}
+
 
 function input_element_top(index) {
     return 10 + (5 + input_element_height) * (index - 1)
@@ -93,6 +279,78 @@ function show_tips() {
 }
 
 
+function jpg_or_not() {
+    return new Promise(function (resolve, reject) {
+        resolve("Whatever");
+        if (user_data === undefined) {
+
+
+            $ui.alert({
+                title: $l10n("Please select jpg files"),
+                message: $l10n("This script uses piexifjs library to modify exif and does only support JPG and JPEG."),
+                actions: [
+                    {
+                        title: $l10n("Exit"),
+                        handler: function () {
+                            utils.stopScript();
+                        }
+                    }
+                ]
+            });
+
+
+        } else {
+            resolve("Whatever");
+        }
+    });
+}
+
+
+function use_default_exif_or_not() {
+    return new Promise(function (resolve, reject) {
+
+        if ((exifObj["GPS"]["1"] && exifObj["0th"]["306"] && exifObj["Exif"]["36867"])) {
+            resolve("Whatever");
+        } else {
+            $ui.alert({
+                title: $l10n("Incomplete exif data"),
+                message: $l10n("Missing exif info will be replaced with default data and you can modify if you want."),
+                actions: [
+                    {
+                        title: "OK",
+                        handler: function () {
+                            if (!(exifObj["GPS"]["1"])) {
+                                exifObj["GPS"] = default_exifObj["GPS"];
+                            }
+
+                            if (!(exifObj["0th"]["306"])) {
+                                exifObj["0th"] = default_exifObj["0th"];
+                            }
+
+                            if (!(exifObj["Exif"]["36867"])) {
+                                exifObj["Exif"] = default_exifObj["Exif"];
+                            }
+
+
+                            // exifObj["1st"] = exifObj["1st"] || default_exifObj["1st"];
+                            resolve("Whatever");
+                        }
+                    },
+                    {
+                        title: "Exit",
+                        handler: function () {
+                            utils.stopScript();
+                        }
+                    }
+                ]
+            })
+        }
+
+
+    });
+}
+
+
 function store_original_exif(original_photo_data) {
 
     return new Promise(function (resolve, reject) {
@@ -101,32 +359,52 @@ function store_original_exif(original_photo_data) {
         console.log("image_data_base64:");
         console.log(image_data_base64);
 
-        exifObj = piexif.load(image_data_base64);
-        console.log("exifObj:");
-        console.log(exifObj);
-        // $ui.alert("adsfsad");
-        console.log($l10n("Device Maker:"));
-        console.log(exifObj["0th"]["271"]);
+        try {
+            exifObj = piexif.load(image_data_base64);
 
-        console.log($l10n("Device Model:"));
-        console.log(exifObj["0th"]["272"]);
+            // $ui.alert(exifObj);
+            use_default_exif_or_not().then(function () {
+                console.log("exifObj:");
+                console.log(exifObj);
+                // $ui.alert("adsfsad");
+                console.log($l10n("Device Maker:"));
+                console.log(exifObj["0th"]["271"]);
 
-        console.log($l10n("Lens Details:"));
-        console.log(exifObj["Exif"]["42036"]);
+                console.log($l10n("Device Model:"));
+                console.log(exifObj["0th"]["272"]);
 
-        console.log($l10n("Create Time:"));
-        console.log(exifObj["0th"]["306"]);
+                console.log($l10n("Lens Details:"));
+                console.log(exifObj["Exif"]["42036"]);
 
-        console.log($l10n("Create Time:"));
-        console.log(exifObj["0th"]["306"]);
+                console.log($l10n("Create Time:"));
+                console.log(exifObj["0th"]["306"]);
 
-        console.log($l10n("Latitude:"));
-        console.log($l10n(exifObj["GPS"]["1"]));
-        console.log(exifObj["GPS"]["2"]["0"]["0"] + "°" + exifObj["GPS"]["2"]["1"]["0"] + "\'" + exifObj["GPS"]["2"]["2"]["0"] + "\"");
-        console.log($l10n("Longitude:"));
+                console.log($l10n("Create Time:"));
+                console.log(exifObj["0th"]["306"]);
 
-        console.log(exifObj["GPS"]["4"]["0"]["0"] + "°" + exifObj["GPS"]["4"]["1"]["0"] + "\'" + exifObj["GPS"]["4"]["2"]["0"] + "\"");
-        resolve("Whatever");
+                console.log($l10n("Latitude:"));
+                console.log($l10n(exifObj["GPS"]["1"]));
+                console.log(exifObj["GPS"]["2"]["0"]["0"] + "°" + exifObj["GPS"]["2"]["1"]["0"] + "\'" + exifObj["GPS"]["2"]["2"]["0"] + "\"");
+                console.log($l10n("Longitude:"));
+
+                console.log(exifObj["GPS"]["4"]["0"]["0"] + "°" + exifObj["GPS"]["4"]["1"]["0"] + "\'" + exifObj["GPS"]["4"]["2"]["0"] + "\"");
+                resolve("Whatever");
+            })
+        } catch (e) {
+            $ui.alert({
+                title: $l10n("Please select jpg files"),
+                message: $l10n("This script uses piexifjs library to modify exif and does only support JPG and JPEG."),
+                actions: [
+                    {
+                        title: $l10n("Exit"),
+                        handler: function () {
+                            utils.stopScript();
+                        }
+                    }
+                ]
+            });
+
+        }
     });
 }
 
@@ -144,6 +422,7 @@ function generate_new_photo_with_original_exif(compressed_photo_data_without_exi
 
 const exifModifyView = {
     props: {
+        id: "main_view",
         title: $l10n("EXIF Modify"),
     },
     views: [
@@ -334,14 +613,17 @@ const exifModifyView = {
             },
             events: {
                 tapped: function (sender) {
-                    $ui.push(timeModifyView);
+                    // $ui.push(timeModifyView);
+                    $("main_view").add(timeModifyView);
+                    // $delay(2, function () {
+                    //     $("time_modify_view").remove();
+                    //
+                    // });
                     $("photo_date_picker").date = photo_date;
 
                 }
             }
         },
-
-
 
 
         // map
@@ -356,8 +638,9 @@ const exifModifyView = {
             },
             layout: function (make, view) {
                 make.top.equalTo(input_element_top(6));
+                // make.bottom.equalTo(deviceScreenHeight - 20);
                 make.left.equalTo(0);
-                make.height.equalTo(deviceScreenHeight * 0.4);
+                make.height.equalTo(deviceScreenHeight - input_element_top(6));
                 make.width.equalTo(deviceScreenWidth * 1);
 
             }
@@ -366,14 +649,15 @@ const exifModifyView = {
             type: "button",
             props: {
                 id: "modify_gps_button",
-                title: $l10n("Tap & Modify"),
+                // title: $l10n("Tap & Modify"),
+                icon: $icon("129", $color("white"), $size(28, 28)),
             },
             layout: function (make) {
 
                 make.top.equalTo(input_element_top(6));
                 make.left.equalTo(deviceScreenWidth * 0.01);
-                make.height.equalTo(deviceScreenHeight * 0.05);
-                make.width.equalTo(deviceScreenWidth * 0.30);
+                make.height.equalTo(50);
+                make.width.equalTo(50);
 
             },
             events: {
@@ -405,8 +689,6 @@ const exifModifyView = {
                                     exifObj["GPS"]["4"]["2"]["0"]
                                 )
                             };
-
-
                         }
                     })
                 }
@@ -422,9 +704,9 @@ const exifModifyView = {
                 title: $l10n("Save & Exit"),
             },
             layout: function (make) {
-                make.top.equalTo(deviceScreenHeight * 0.75);
+                make.top.equalTo(deviceScreenHeight * 0.82);
                 // make.left.equalTo(deviceScreenWidth * 0.23);
-                make.centerX.equalTo(0).offset(9);
+                make.centerX.equalTo(0).offset(0);
                 make.height.equalTo(input_element_height);
                 make.width.equalTo(deviceScreenWidth * 0.618 / 2);
             },
@@ -456,17 +738,68 @@ const exifModifyView = {
                 }
             }
         },
-
-
     ]
 };
 
 
 const timeModifyView = {
     props: {
-        title: $l10n("EXIF Modify"),
+        title: $l10n("Time Modify"),
+        id: "time_modify_view"
+    },
+    layout: function (make, view) {
+        make.top.equalTo(0);
+        make.left.equalTo(0);
+        make.height.equalTo(deviceScreenHeight);
+        make.width.equalTo(deviceScreenWidth);
     },
     views: [
+        {
+            type: "view",
+            props: {
+                bgcolor: $color("#E6E6FA"),
+                // smoothRadius: 80,
+                // opaque: false,
+                // tintColor: $color("#fa754a"),
+                // borderWidth: 1,
+                // borderColor: $color("#4b96fa"),
+                alpha: 0.75
+            },
+            layout: function (make, view) {
+                make.top.equalTo(0);
+                make.left.equalTo(0);
+                make.height.equalTo(deviceScreenHeight);
+                make.width.equalTo(deviceScreenWidth);
+            },
+            events: {
+                tapped: function (sender) {
+
+                }
+            }
+        },
+        {
+            type: "view",
+            props: {
+                bgcolor: $color("#E6E6FA"),
+                smoothRadius: 80,
+                // opaque: false,
+                // tintColor: $color("#fa754a"),
+                // borderWidth: 1,
+                borderColor: $color("#4b96fa"),
+                alpha: 0.618
+            },
+            layout: function (make, view) {
+                make.top.equalTo(deviceScreenHeight * 0.15);
+                make.left.equalTo(deviceScreenWidth * 0.19);
+                make.height.equalTo(deviceScreenHeight * 0.4);
+                make.width.equalTo(deviceScreenWidth * 0.98 - deviceScreenWidth * 0.19);
+            },
+            events: {
+                tapped: function (sender) {
+
+                }
+            }
+        },
         {
             type: "date-picker",
             props: {
@@ -475,10 +808,10 @@ const timeModifyView = {
                 date: new Date(),
             },
             layout: function (make) {
-                make.top.equalTo(20);
-                make.left.equalTo(deviceScreenWidth * 0.07);
-                make.height.equalTo(deviceScreenHeight * 0.7);
-                make.width.equalTo(deviceScreenWidth * 0.86);
+                make.top.equalTo(deviceScreenHeight * 0.15);
+                make.left.equalTo(deviceScreenWidth * 0.19);
+                make.height.equalTo(deviceScreenHeight * 0.4);
+                make.width.equalTo(deviceScreenWidth * 0.98 - deviceScreenWidth * 0.19);
             },
             events: {
                 changed: function (sender) {
@@ -517,18 +850,18 @@ const timeModifyView = {
             type: "button",
             props: {
                 id: "save_time_button",
-                title: $l10n("Save & Return"),
+                title: $l10n("OK"),
             },
             layout: function (make) {
-                make.top.equalTo(deviceScreenHeight * 0.75);
-                // make.left.equalTo(deviceScreenWidth * 0.23);
-                make.centerX.equalTo(0).offset(9);
+                make.top.equalTo(deviceScreenHeight * 0.52);
+                make.left.equalTo(deviceScreenWidth * 0.50);
+                // make.centerX.equalTo(0).offset(9);
                 make.height.equalTo(input_element_height);
-                make.width.equalTo(deviceScreenWidth * 0.45);
+                make.width.equalTo(deviceScreenWidth * 0.18);
             },
             events: {
                 tapped: function (sender) {
-                    $ui.pop();
+                    $("time_modify_view").remove();
                 }
             }
         }
@@ -580,6 +913,9 @@ if (($app.env !== $env.app)) {
                     $("device_model_input").text = exifObj["0th"]["272"];
                     $("lens_model_input").text = exifObj["Exif"]["42036"];
                     $("resolution_label").text = exifObj["Exif"]["40963"] + " * " + exifObj["Exif"]["40962"];
+                    if(exifObj["Exif"]["40963"] === 1 && exifObj["Exif"]["40962"] ===1){
+                        $("resolution_label").text = "";
+                    }
 
 
                     $("modify_time_button").title = photo_date_string.slice(0, 4)
@@ -612,23 +948,10 @@ if (($app.env !== $env.app)) {
                         exifObj["GPS"]["2"]["1"]["0"],
                         exifObj["GPS"]["2"]["2"]["0"]
                     ));
-
-
-
                 })
             }
         })
-
     })
-
 }
-
-
-
-
-
-
-
-
 
 
